1b26dd8c7f20758ab4325e6c291918a8011904cf
byunn90 <kayhanturk90@Live.com>
2023-05-23 00:56:40 +1000

    count with use effect with condition

--- my-app/src/MyApp.js
+++ my-app/src/MyApp.js
@@ -1,18 +1,36 @@
 import React, { useState, useEffect } from "react";
-import MyFooter from "./components/footer";
 
 function MyApp() {
   const [count, setCount] = useState(0);
 
   useEffect(() => {
-    document.title = `You clicked ${count} times`;
+    document.title = `You clicked using ${count}`;
   });
+
+  function increamentClick() {
+    setCount(count + 1);
+  }
+
+  const decreamentClick = () => {
+    setCount(count - 1);
+  };
+
+  useEffect(() => {
+    if (count <= 0) {
+      // Stop decrementing at 0
+      setCount(0);
+    } else if (count >= 10) {
+      // Stop incrementing at 10
+      setCount(10);
+    }
+  }, [count]);
 
   return (
     <div>
       <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>Click me</button>
-      <MyFooter />
+      <button onClick={increamentClick}>Increase</button>
+      <span>{count}</span>
+      <button onClick={decreamentClick}>Decrease me</button>
     </div>
   );
 }
